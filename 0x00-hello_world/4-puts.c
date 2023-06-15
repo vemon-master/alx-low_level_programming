b1ec6e0350d8da4ea869f83e6b2ec5007ddfd2fd
vemon-master <linuscode.linus@gmail.com>
2023-06-15 01:00:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#iclude < stdio.h >
+/**
+ * main - Entry point
+ *
+ * Return: Programming is like building a multilingua puzzle
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
