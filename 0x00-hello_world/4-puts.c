65ff31435359819933acc37e01b7bbf3ffbe29f5
vemon-master <linuscode.linus@gmail.com>
2023-06-15 01:13:45 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#include < stdio.h >
 /**
  * main - Entry point
  *
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
