1f1b9d150904d598704737da783dd17df52aac5e
vemon-master <133804352+vemon-master@users.noreply.github.com>
2023-06-15 16:11:17 +0300

    Update 101-quote.c
    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry input
- *
  * return 1 (success)
  */
-
-/*main function starts here*/
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
