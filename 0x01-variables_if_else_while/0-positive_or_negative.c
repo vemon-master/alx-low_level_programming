1c30f80785c56d5b42b1f3be43994160127e5f09
vemon-master <linuscode.linus@gmail.com>
2023-06-16 05:14:47 -0700

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
- * main - entry point
- *
- * Return 0 (success)
- * in this program we are creating a code that generates
- * random numbers and tells us if its negative, positive or equal to zero
+ * main - Entry point
+ * Return: Always return 0 (Success)
  */
 
 int main(void)/* the main function starts here */
