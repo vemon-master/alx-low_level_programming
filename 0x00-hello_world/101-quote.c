2a67bc29dfdd876063b817793a208cd5025d2db8
vemon-master <133804352+vemon-master@users.noreply.github.com>
2023-06-16 22:03:28 +0300

    Update 101-quote.c
    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry input
- * return 1 (success)
+ * main - Entry point
+ * Return: Aways return 0 (Success)
  */
+
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
