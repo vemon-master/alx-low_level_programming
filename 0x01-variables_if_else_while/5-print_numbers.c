cd08b65444a500b4182e3ca694bb0abae68eeed1
vemon-master <linuscode.linus@gmail.com>
2023-06-16 09:28:25 -0700

    5-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Aways return 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9 ; c++)
+		printf("%d", c);
+	return (0);
+}
