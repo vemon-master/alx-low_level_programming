9049f3a5bf18fbac4a81c801dac651ae8148d1d0
vemon-master <linuscode.linus@gmail.com>
2023-06-16 07:42:42 -0700

    Osto dog

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Aways return 0 (Success)
+ */
+int main(void)
+{
+	char o;
+
+	for (o = 'a' ; o <= 'z' ; o++)
+
+	{
+		if (o == 'q' || o == 'e')
+
+			continue;
+
+		putchar(o);
+	}
+	putchar('\n')
+	return (0);
+}
