cb0a258574a21838d70fbee1c1e9353cd42d0bb1
vemon-master <linuscode.linus@gmail.com>
2023-06-16 09:51:35 -0700

    6-numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Aways return 0 (Success)
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 0 ; l <= 9 ; l++)
+		putchar('0' + l);
+	putchar('\n');
+	return (0);
+}
