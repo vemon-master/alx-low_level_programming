35cd575ead2dabb6f4082af989252bd69cde4bf2
vemon-master <linuscode.linus@gmail.com>
2023-06-16 06:19:20 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Aways return 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l='a' ; l<='z' ; l++)
+	putchar(l);
+
+	for (l='A' ; l<='Z' ; l++)
+	putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
